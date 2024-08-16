11945581caf28f2dbc2d5b4fe7eb9ae5225bc8ab
vitayusko <vitayusko@users.noreply.github.com>
2024-08-16 13:22:13 +0200

    ragistartion completed

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { registerThunk } from "./operations";
 
 const initialState = {
   user: {
@@ -13,6 +14,13 @@ const initialState = {
 const slice = createSlice({
   name: "auth",
   initialState,
+  extraReducers: (builder) => {
+    builder.addCase(registerThunk.fulfilled, (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    });
+  },
 });
 
 export const authReducer = slice.reducer;
