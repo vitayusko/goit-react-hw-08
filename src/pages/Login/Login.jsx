c50a3cf08393e070e898ea674be2315145b30e14
vitayusko <vitayusko@users.noreply.github.com>
2024-08-16 11:42:08 +0200

    refiatration and login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,42 @@
+import { Field, Form, Formik } from "formik";
 import React from "react";
+import s from "./Login.module.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const handleSubmit = (values, options) => {
+    console.log(values);
+    options.resetForm();
+  };
+  return (
+    <div>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className={s.form}>
+          <Field
+            name="email"
+            placeholder="Enter your email"
+            className={s.input}
+          />
+          <Field
+            name="password"
+            placeholder="Enter your password"
+            className={s.input}
+          />
+          <button type="submit" className={s.btn}>
+            Login
+          </button>
+          <p>
+            You don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,47 @@
+import { Field, Form, Formik } from "formik";
 import React from "react";
+import { Link } from "react-router-dom";
+import s from "./Register.module.css";
 
 const Register = () => {
-  return <div>Register</div>;
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
+  const handleSubmit = (values, options) => {
+    console.log(values);
+    options.resetForm();
+  };
+  return (
+    <div>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className={s.form}>
+          <Field
+            name="name"
+            placeholder="Enter your name"
+            className={s.input}
+          />
+          <Field
+            name="email"
+            placeholder="Enter your email"
+            className={s.input}
+          />
+          <Field
+            name="password"
+            placeholder="Enter your password"
+            className={s.input}
+          />
+          <button type="submit" className={s.btn}>
+            Login
+          </button>
+          <p>
+            You already have an account <Link to="/login">Sign In</Link>
+          </p>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default Register;
